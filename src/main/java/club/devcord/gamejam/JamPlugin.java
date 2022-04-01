98190f61f9e8598169e20991531a3ee56d3a5f5c
Yannick Lamprecht <yannicklamprecht@live.de>
2022-04-01 18:30:47 +0200

    initial commit

---
+++ src/main/java/club/devcord/gamejam/JamPlugin.java
@@ -0,0 +1,17 @@
+package club.devcord.gamejam;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JamPlugin extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
